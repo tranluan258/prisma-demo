46d2f3332a9c9394837e6341ea53b41525bd9c5c
Tran Luan <tranluanqqq@gmail.com>
2022-11-05 19:50:51 +0700

    test deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,22 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'PrismaDemo',
     script: './dist/main.js',
     watch: '.',
     autorestart: true
   }],
 
-  deploy : {
-    production : {
-      ref  : 'origin/master',
-      repo : 'https://github.com/tranluan258/prisma-demo.git',
-      path : '.',
+  deploy: {
+    production: {
+      user: 'tluan',
+      host: '192.168.1.99',
+      key: 'deploy.key',
+      ref: 'origin/master',
+      repo: 'https://github.com/tranluan258/prisma-demo.git',
+      path: '/home/tluan/prisma-demo',
       'post-setup': 'yarn add && yarn build &&  pm2 start ecosystem.config.js --env production',
-      'post-deploy' : 'yarn add && yarn build &&  pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'yarn add && yarn build &&  pm2 reload ecosystem.config.js --env production',
       "ssh_options": [
         "StrictHostKeyChecking=no",
         "PasswordAuthentication=no"
